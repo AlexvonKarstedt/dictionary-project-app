42c2dc1e8b96f89f15274f518c5ab8ede92572e4
AlexvonKarstedt <80624199+AlexvonKarstedt@users.noreply.github.com>
2021-05-28 17:20:10 -0400

    Created component Synonym.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,7 +15,7 @@ export default function Dictionary () {
 
     function search (event) {
     event.preventDefault()
-    alert (`Searching for ${keyword} definition...`);
+    
 
     //documentation: https://dictionaryapi.dev
 
@@ -23,8 +23,6 @@ let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 console.log(apiUrl);
 }
-
-
 
 function handleKeywordChange(event) {
     setKeyword(event.target.value);
