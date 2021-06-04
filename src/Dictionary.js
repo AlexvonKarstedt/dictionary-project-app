d5a5d7b1ba617552752b61efc5f720feb7c16524
AlexvonKarstedt <80624199+AlexvonKarstedt@users.noreply.github.com>
2021-06-03 22:50:33 -0400

    added Pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,27 @@ export default function Dictionary (props) {
     let [keyword, setKeyword] = useState (props.defaultKeyword);
     let [results, setResults] = useState (null);
     let [loaded, setLoaded] = useState (false)
+    let [pexels, setPexels] = useState(" ")
+    
     function handleResponse (response) {
         setResults(response.data[0]);
     }
+
+    function handlePexelsResponse (response) {
+        console.log (response.data);
+    }
+
     function search () {
 //documentation: https://dictionaryapi.dev
 let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
-    }
+   
+let pexelsApiKey = "563492ad6f91700001000001f6b09fa8df8946b2a478ffa9b88286c4";
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+let headers = {"Authorization": `Bearer ${pexelsApiKey}`}
+axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
+
+}
 
     function handleSubmit (event) {
     event.preventDefault()
